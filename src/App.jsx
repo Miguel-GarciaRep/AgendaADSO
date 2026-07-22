1a32da85c6813661df1579006ec69e7adfd53f9c
MiguelAngel <argelts1403@gmail.com>
2026-07-21 22:30:28 -0500

    Versión inicial - Componente Hola

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+export default function App() {
+  const fecha = new Date().toLocaleString();
+
+  return (
+    <main>
+      <h1>Hola profe. Soy Migue Angel Garcia Londoño</h1>
+      <p>Lo que espero que pase en este trimestre con su materia/competencia, es poder llegar a extender
+        mi conocimiento, hacerme un profesional en el tema y graduarme con honores para entrar en una empresa
+        y convertirme en un exitoso</p>
+    </main>
+  )
+}
