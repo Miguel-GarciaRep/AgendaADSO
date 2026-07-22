4669afc05220aa6b14ca6c3f3eaab3db8cc0335f
MiguelAngel <argelts1403@gmail.com>
2026-07-21 23:27:06 -0500

    Personalización del componente App

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ export default function App() {
 
   return (
     <main>
-      <h1>Hola profe. Soy Migue Angel Garcia Londoño</h1>
+      <h1>Hola profe. Soy Miguel Angel Garcia Londoño</h1>
       <p>Lo que espero que pase en este trimestre con su materia/competencia, es poder llegar a extender
         mi conocimiento, hacerme un profesional en el tema y graduarme con honores para entrar en una empresa
         y convertirme en un exitoso</p>
+      <p>
+        Y lo que mas me gustaria hacer, es estudiar programacion de videojuegos y dedicarme a crear juegos
+        indie o en una empresa dedicada a ello.
+      </p>
     </main>
   )
 }
